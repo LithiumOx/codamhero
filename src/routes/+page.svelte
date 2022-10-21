0a3ed9c8f74e28991bfe751916a1cac9407e60e4
Mees Dekker <meesdekker2622002@gmail.com>
2022-10-21 23:17:17 +0200

    Updated readme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,44 @@
 <script>
-import Card from '$lib/components/card.svelte';
-let cards = [{
-        title: 'Cluster map',
-        desc: "A map of all students currently in Codam",
-        version: "0.2",
-        icon: "🗺️",
-        href: "1"
-    },
-    {
-        title: 'Students',
-        desc: "A list of all students at Codam",
-        version: "0.2",
-        icon: "🎓",
-		href: '2'
-    },
-    {
-        title: 'Coalitions',
-        desc: "Statistics on all coalitions at Codam",
-        version: "0.2",
-        icon: "🪄",
-		href: '2'
-    },
-    {
-        title: 'Staff',
-        desc: "A list of all staff at Codam",
-        version: "0.2",
-        icon: "👨‍🏫",
-		href: '2'
-    }
-]
+	import Card from '$lib/components/card.svelte';
+	let cards = [
+		{
+			title: 'Cluster map',
+			desc: 'A map of all students currently in Codam',
+			version: '0.2',
+			icon: '🗺️',
+			href: '1'
+		},
+		{
+			title: 'Students',
+			desc: 'A list of all students at Codam',
+			version: '0.2',
+			icon: '🎓',
+			href: '2'
+		},
+		{
+			title: 'Coalitions',
+			desc: 'Statistics on all coalitions at Codam',
+			version: '0.2',
+			icon: '🪄',
+			href: '2'
+		},
+		{
+			title: 'Staff',
+			desc: 'A list of all staff at Codam',
+			version: '0.2',
+			icon: '👨‍🏫',
+			href: '2'
+		}
+	];
 </script>
 
 <svelte:head>
-    <title>Home</title>
-    <meta name="description" content="Better version of the Codam hero site." />
+	<title>CodamHero</title>
+	<meta name="description" content="my version of the Codam hero site." />
 </svelte:head>
 
-    <!-- <div class="mx-auto">
-
-<h1 class="text-6xl text-center font-black">Hero</h1>
-    </div> -->
-
-    <menu class="grid m-5 grid-cols-2 gap-5 my-auto">
-		{#each cards as data}
-			<Card {...data} />
-		{/each}
-    </menu>
+<menu class="grid m-5 grid-cols-2 gap-5 my-auto">
+	{#each cards as data}
+		<Card {...data} />
+	{/each}
+</menu>
